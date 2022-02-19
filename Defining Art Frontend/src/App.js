0b52ce6a86ea334b53ae27f1807d640713eea901
Sarthak Agarwal <sarthakagarwal22@gmail.com>
2022-02-19 12:24:55 +0530

    Removed warnings

--- Defining Art Frontend/src/App.js
+++ Defining Art Frontend/src/App.js
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import {
   ConnectWalletAPI,
-  DisconnectWalletAPI,
   FetchWalletAPI,
 } from './beacon-functions';
 
 import {useWalletAddressUpdate} from './Context/walletContext';
-import { fetchPlentyBalanceOfUser, transferPlenty } from './taquito-functions';
 
 import ConnectWallet from './Components/ConnectWallet/ConnectWallet';
 import Homepage from './Components/Homepage/homepage';
 
 function App() {
   const [walletAddress, setWalletAddress] = useState('');
-  const [plentyHolderAddress, setPlentyHolderAddress] = useState('');
-  const [plentyHolderBalance, setPlentyHolderBalance] = useState(0);
-  const [balanceLoading, setBalanceLoading] = useState(false);
-  const [amount, setAmount] = useState(0);
-  const [receiverAddress, setReceiverAddress] = useState('');
-  const [transferLoading, setTransferLoading] = useState(false);
   const updateWalletAddress = useWalletAddressUpdate()
 
   useEffect(() => {
@@ -26,56 +18,8 @@ function App() {
       setWalletAddress(resp.wallet);
       updateWalletAddress(resp.wallet);
     });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const plentyHolderInputHandler = (value) => {
-    setPlentyHolderAddress(value);
-  };
-
-  const amountInputHandler = (value) => {
-    setAmount(parseFloat(value));
-  };
-
-  const receiverAddressInputHandler = (value) => {
-    setReceiverAddress(value);
-  };
-
-  const transferPlentyHandler = () => {
-    setTransferLoading(true);
-    transferPlenty(amount, receiverAddress)
-      .then((resp) => {
-        console.log(resp);
-        if (resp.success) {
-          alert('Transfer Successfull with operation Id: ' + resp.opID);
-          setTransferLoading(false);
-        } else {
-          alert('Transfer Failed');
-          setTransferLoading(false);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        alert('Some error occured');
-        setTransferLoading(false);
-      });
-  };
-
-  const fetchBalanceHandler = () => {
-    setBalanceLoading(true);
-    fetchPlentyBalanceOfUser(plentyHolderAddress)
-      .then((resp) => {
-        console.log(resp.userBalance);
-        alert('Plenty Balance is ' + resp.userBalance);
-        setBalanceLoading(false);
-        setPlentyHolderBalance(resp.userBalance);
-      })
-      .catch((err) => {
-        console.log(err);
-        setBalanceLoading(false);
-        alert('Plenty Balance is ' + 0);
-        setPlentyHolderBalance(0);
-      });
-  };
 
   const connectWalletHandler = () => {
     ConnectWalletAPI()
