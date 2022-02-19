ccc3899c4441f35d277bab49a55a1afe03bfafd1
Nupur Rajal <nupur.rajal@swiggy.in>
2022-02-20 01:45:23 +0530

    added twitter api, mail changes

--- deFining-art-BE/routes/images.js
+++ deFining-art-BE/routes/images.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getImagesFromDb, setImageInDb} from '../services/images.js'
+import {getImagesFromDb, setImageInDb, getAIGeneratedImageDetails} from '../services/images.js'
 
 const imagesRouter = express.Router();
 
@@ -20,14 +20,32 @@ imagesRouter.post('/setondate', async (req, res, next) => {
         next(new Error('Invalid request'));
     }
     try{
-    await setImageInDb(req.body.date, req.body.imageData);
-    res.status(200).send({
-        status: "OK",
-        message: "Image updated successfully"
-    });
+        await setImageInDb(req.body.date, req.body.imageData);
+        res.status(200).send({
+            status: "OK",
+            message: "Image updated successfully"
+        });
     }catch(e){
         next(e);
     }
+})
+
+imagesRouter.post('/getGenerateImages', async (req, res, next) => {
+    if(!(req && req.body)){
+        next(new Error('Invalid request'));
+    };
+    let aiGenerateImageDetails;
+    try {
+        aiGenerateImageDetails = await getAIGeneratedImageDetails(req.body.date);
+    } catch (e) {
+        next(e);
+    }
+    res.status(200).send({
+        status: "OK",
+        message: "Image fetched successfully",
+        imageDetails: aiGenerateImageDetails
+    });
+    
 })
 
 export default imagesRouter;
