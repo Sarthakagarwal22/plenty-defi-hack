327ce084d6089cfc79fb9e3f05ed0e3bfbccd740
Sarthak Agarwal <sarthakagarwal22@gmail.com>
2022-02-19 13:27:59 +0530

    Removed dotenv from mongoose

--- deFining-art-BE/db/moongoose.js
+++ deFining-art-BE/db/moongoose.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export const eventsDB = mongoose.createConnection(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@definingart.0lck4.mongodb.net/calendar?retryWrites=true&w=majority`, {
     useNewUrlParser: true, 
